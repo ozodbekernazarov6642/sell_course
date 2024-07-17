c99bb9cfeea89c512ab347fdaa67d17e9c41df78
ozodbekernazarov6642 <128281409+ozodbekernazarov6642@users.noreply.github.com>
2024-07-17 18:21:20 +0500

    Update config.py

--- data/config.py
+++ data/config.py
@@ -5,7 +5,7 @@ BOT_TOKEN = '6975533676:AAF51ezr9N3CDdnMqYMVQVg-9d8m_QDuZkY'  # Bot toekn
 ADMINS = [6089744035]  # adminlar ro'yxati
 
 DB_USER = 'sell_course_owner'
-DB_PASS = 'M7UIiPQ3rJuw'
+DB_PASS = 'TirKVbSn03RF'
 DB_NAME = 'sell_course'
 DB_HOST = 'ep-patient-term-a5d8qhcl.us-east-2.aws.neon.tech'
 
