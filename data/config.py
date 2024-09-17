7197fbde42ef7da0eea58ebe7c32984e64b523dd
Sar-dor <sardorernazarov4555@gmail.com>
2024-09-17 22:25:21 +0500

    bir

--- data/config.py
+++ data/config.py
@@ -1,7 +1,6 @@
 from environs import Env
 
 # .env fayl ichidan quyidagilarni o'qiymiz
-<<<<<<< HEAD
 BOT_TOKEN = '6989708033:AAElDGLT2t9nNObxPeBS-yohs5_k646yHzE'  # Bot toekn
 ADMINS = [6642693329] # adminlar ro'yxati
 
@@ -9,14 +8,8 @@ DB_USER = 'sell_course'
 DB_PASS = '6DOGuMv2aPco'
 DB_NAME = 'downloader'
 DB_HOST = 'ep-patient-term-a5d8qhcl.us-east-2.aws.neon.tech'
-=======
-BOT_TOKEN = '6975533676:AAF51ezr9N3CDdnMqYMVQVg-9d8m_QDuZkY'  # Bot toekn
-ADMINS = [6089744035]  # adminlar ro'yxati
-
-DB_USER = 'sell_course_owner'
-DB_PASS = 'TirKVbSn03RF'
-DB_NAME = 'sell_course'
-DB_HOST = 'ep-patient-term-a5d8qhcl.us-east-2.aws.neon.tech'
 
 
->>>>>>> c99bb9cfeea89c512ab347fdaa67d17e9c41df78
+
+
+
